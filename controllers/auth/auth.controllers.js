d3bbb85371ac8d85db92909156b24e1aa6dee870
vannydevelopper <vannydevelopper>
2024-10-24 16:27:24 +0200

    ajout de nouveau fonctionalite pour les liaison entre les tables

--- controllers/auth/auth.controllers.js
+++ controllers/auth/auth.controllers.js
@@ -3,13 +3,62 @@ const express = require("express");
 const RESPONSE_STATUS = require('../../constants/RESPONSE_STATUS');
 const RESPONSE_CODES = require('../../constants/RESPONSE_CODES');
 const { supabase } = require("../../utils/supabase");
-// const md5 = require("md5");
 // const generateToken = require("../../utils/generateToken");
 
 
+const loginUsers = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        // Validation des données d'entrée
+        if (!email || !password) {
+            return res.status(RESPONSE_CODES.BAD_REQUEST).json({
+                statusCode: RESPONSE_CODES.BAD_REQUEST,
+                httpStatus: RESPONSE_STATUS.BAD_REQUEST,
+                message: "Tous les champs sont requis.",
+            });
+        }
+        // Récupérer l'utilisateur avec l'e-mail correspondant
+        const { data: user, error } = await supabase
+            .from('users')
+            .select('*')
+            .eq('email', email)
+            .single(); // Utiliser .single() pour obtenir un seul utilisateur
+
+        if(!error || user){
+            if (password !== user.password) {
+                return res.status(RESPONSE_CODES.UNAUTHORIZED).json({
+                    statusCode: RESPONSE_CODES.UNAUTHORIZED,
+                    httpStatus: RESPONSE_STATUS.UNAUTHORIZED,
+                    message: "Identifiants invalides.",
+                });
+            }
+            res.status(RESPONSE_CODES.OK).json({
+                statusCode: RESPONSE_CODES.OK,
+                httpStatus: RESPONSE_STATUS.OK,
+                message: "Connexion réussie.",
+                result: user,
+            });
+        }else{
+            return res.status(RESPONSE_CODES.UNAUTHORIZED).json({
+                statusCode: RESPONSE_CODES.UNAUTHORIZED,
+                httpStatus: RESPONSE_STATUS.UNAUTHORIZED,
+                message: "Identifiants invalides.",
+            });
+        }
+    }
+    catch (error) {
+        console.log(error);
+        res.status(RESPONSE_CODES.INTERNAL_SERVER_ERROR).json({
+            statusCode: RESPONSE_CODES.INTERNAL_SERVER_ERROR,
+            httpStatus: RESPONSE_STATUS.INTERNAL_SERVER_ERROR,
+            message: "Erreur interne du serveur, réessayer plus tard",
+        });
+    }
+};
+
 const getAllData = async (req, res) => {
     try {
-        const user = await supabase.from('users').select('*')
+        const user = await supabase.from('livres').select('*, auteur(id, nom, prenom,telophone,email)');
         res.status(RESPONSE_CODES.CREATED).json({
             statusCode: RESPONSE_CODES.CREATED,
             httpStatus: RESPONSE_STATUS.CREATED,
@@ -83,5 +132,6 @@ const deleteUsers = async (req, res) => {
 module.exports = {
     getAllData,
     createUsers,
-    deleteUsers
+    deleteUsers,
+    loginUsers
 }
